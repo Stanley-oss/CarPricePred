ee92f5eb37aaf13e712e9d40d6c49f71e4698b4a
Liu Zhen <AIT2409033@xmu.edu.my>
2025-12-23 17:11:37 +0800

    Modify API_URL

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { CarFormData, PredictionResponse } from './types';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = '';
 
 export const predictPrice = async (data: CarFormData): Promise<PredictionResponse> => {
   const response = await axios.post<PredictionResponse>(`${API_URL}/predict`, data);
